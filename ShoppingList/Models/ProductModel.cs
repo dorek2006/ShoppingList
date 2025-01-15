49848cc40a96dae90b8cfb8e48d46fd5aca4febd
dorek2006 <92580789+dorek2006@users.noreply.github.com>
2025-01-15 22:53:30 +0100

    kon

---
+++ ShoppingList/Models/ProductModel.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel;
+using ShoppingList.Models;
+
+namespace ShoppingList.Models
+{
+    public class ProductModel : INotifyPropertyChanged
+    {
+        public string Name { get; set; }
+        public string Unit { get; set; } 
+
+        private bool _isPurchased;
+        public bool IsPurchased
+        {
+            get => _isPurchased;
+            set
+            {
+                if (_isPurchased != value)
+                {
+                    _isPurchased = value;
+                    OnPropertyChanged(nameof(IsPurchased));
+                }
+            }
+        }
+
+        private int _Quantity;
+        public int Quantity
+        {
+            get => _Quantity;
+            set
+            {
+                if (_Quantity != value)
+                {
+                    _Quantity = value;
+                    OnPropertyChanged(nameof(Quantity));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
